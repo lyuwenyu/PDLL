48040d90d5e8c2f93f19dee1b72dd787f2819dfd
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-12 16:03:53 +0800

    add mnist example

---
+++ docs/mnist.py
@@ -0,0 +1,127 @@
+import argparse
+import numpy as np 
+
+import _init_path
+import pdll as L
+import pdll.nn as nn
+import pdll.optim as optim
+import pdll.nn.functional as F
+
+import torch
+from torchvision import datasets, transforms
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.reshape(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class Net1(nn.Module):
+    def __init__(self):
+        super(Net1, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 2, 1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, 2, 1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pooling = nn.MaxPool2d(3, 2, 1)
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        n, _, _, _ = x.shape
+        x = self.bn1(self.conv1(x)).relu()
+        x = self.bn2(self.conv2(x)).relu()
+        x = self.pooling(x)
+        x = self.fc1(x.reshape(n, -1)).relu()
+        x = self.fc2(x)
+        return x
+
+
+def train(args, model, train_loader, optimizer, epoch):
+    '''
+    '''
+    model.train()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        data = L.from_numpy(data.data.numpy())
+        target = target.data.numpy()
+        target = L.Variable(np.eye(10)[target])
+
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # optimizer.step()
+
+        for p in model.parameters():
+            p.data = p.data - p.grad * args.lr
+
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * data.shape[0], len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data))
+
+
+def test(model, test_loader):
+    model.eval()
+
+    correct = 0
+    for data, target in test_loader:
+        data = L.from_numpy(data.data.numpy())
+
+        output = model(data)
+        pred = output.data.argmax(dim=1)
+        correct += (pred == target).sum()
+
+    print('\nAccuracy: {}/{} ({:.0f}%)\n'.format(correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--log-interval', type=int, default=5, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+
+    train_kwargs = {'batch_size': args.batch_size, 'shuffle': True}
+    test_kwargs = {'batch_size': args.test_batch_size}
+
+    model = Net1()
+
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
+    dataset1 = datasets.MNIST('../data', train=True, download=False, transform=transform)
+    dataset2 = datasets.MNIST('../data', train=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, train_loader, optimizer, epoch)
+        test(model, test_loader)
+
+if __name__ == '__main__':
+    main()
