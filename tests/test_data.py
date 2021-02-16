7cc90694d97977fcb07637dd0babdbda50baa6c8
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-16 12:27:43 +0800

    test dataset dataloader

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,7 +8,7 @@ import pdll as L
 
 class Testing(unittest.TestCase):
 
-    def test_data(self, ):
+    def test_dataloader(self, ):
         ''' '''
 
         class DB(L.io.dataset.Dataset):
@@ -20,11 +20,24 @@ class Testing(unittest.TestCase):
                 return len(self.data)
 
         dataset = DB()
-        dataloader = L.io.dataloader.DataLoader(dataset, batch_size=10, shuffle=True)
+        dataloader = L.io.dataloader.DataLoader(dataset, batch_size=10, shuffle=False)
 
         for e in range(3):
             for i, batch in enumerate(dataloader):
                 print(e, i, len(batch), batch)
+
+        for i, batch in enumerate(dataloader):
+            print('----', i, len(batch), batch)
+
+
+    def test_dataset(self, ):
+        
+        mnist = L.io.dataset.MNIST(train=False)
+        print('dataloader: ', mnist[0][0].shape)
+        dataloader = L.io.DataLoader(mnist, batch_size=2000)
+
+        for i, data in enumerate(dataloader):
+            print(i, len(data), len(data[1]), np.array(data[0]).shape )
 
 
 if __name__ == '__main__':
