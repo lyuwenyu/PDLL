09ad39ae75421f684886a9cc4fcd5a7acd77f460
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-17 10:06:52 +0800

    add padding tests

---
+++ tests/test_padding.py
@@ -0,0 +1,40 @@
+
+import _init_path
+import unittest
+
+import numpy as np
+import torch
+import pdll as L
+
+class Testing(unittest.TestCase):
+
+    def test_padding_functional(self, ):
+        ''' '''
+        for padding in [2, [2, 3], [2, 3, 4, 5], ]:
+            t = torch.rand(4, 5, 6, 7, requires_grad=True)
+            ot = torch.nn.ZeroPad2d(padding=padding)(t)
+            v = L.from_numpy(t.data.numpy()[...], requires_grad=True)
+            ov = L.nn.functional.zero_pad2d(v, padding)
+            ot.mean().backward()
+            ov.mean().backward()
+
+            np.testing.assert_almost_equal(ov.data, ot.data.numpy())
+            np.testing.assert_almost_equal(v.grad, t.grad.data.numpy())
+
+
+    def test_padding_module(self, ):
+        ''' '''
+        for padding in [2, [2, 3], [2, 3, 4, 5], ]:
+            t = torch.rand(4, 5, 6, 7, requires_grad=True)
+            ot = torch.nn.ZeroPad2d(padding=padding)(t)
+            v = L.from_numpy(t.data.numpy()[...], requires_grad=True)
+            ov = L.nn.ZeroPad2d(padding)(v)
+            ot.mean().backward()
+            ov.mean().backward()
+
+            np.testing.assert_almost_equal(ov.data, ot.data.numpy())
+            np.testing.assert_almost_equal(v.grad, t.grad.data.numpy())
+        
+
+if __name__ == '__main__':
+    unittest.main(['-m', 'add', '-s'])
