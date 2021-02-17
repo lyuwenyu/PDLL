430f6d6d9021d3ec7ba08414f164ded4d83c8041
lyuwenyu <wenyu.lyu@gmail.com>
2021-02-17 15:35:34 +0800

    add zero constant pad2d

--- tests/test_padding.py
+++ tests/test_padding.py
@@ -22,13 +22,29 @@ class Testing(unittest.TestCase):
             np.testing.assert_almost_equal(v.grad, t.grad.data.numpy())
 
 
-    def test_padding_module(self, ):
+    def test_zeropadding_module(self, ):
         ''' '''
         for padding in [2, [2, 3], [2, 3, 4, 5], ]:
             t = torch.rand(4, 5, 6, 7, requires_grad=True)
-            ot = torch.nn.ZeroPad2d(padding=padding)(t)
             v = L.from_numpy(t.data.numpy()[...], requires_grad=True)
-            ov = L.nn.ZeroPad2d(padding)(v)
+
+            ot = torch.nn.ZeroPad2d(padding=padding)(t)
+            ov = L.nn.ZeroPad2d(padding=padding)(v)
+            ot.mean().backward()
+            ov.mean().backward()
+
+            np.testing.assert_almost_equal(ov.data, ot.data.numpy())
+            np.testing.assert_almost_equal(v.grad, t.grad.data.numpy())
+
+
+    def test_constantpadding_module(self, ):
+        ''' '''
+        for padding in [2, [2, 3], [2, 3, 4, 5], ]:
+            t = torch.rand(4, 5, 6, 7, requires_grad=True)
+            v = L.from_numpy(t.data.numpy()[...], requires_grad=True)
+
+            ot = torch.nn.ConstantPad2d(padding=padding, value=2.)(t)
+            ov = L.nn.ConstantPad2d(padding=padding, value=2.)(v)
             ot.mean().backward()
             ov.mean().backward()
 
