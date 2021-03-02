f78eaa813b6c5790d40bcb4a4beb193daac37c6e
lyuwenyu <wenyu.lyu@gmail.com>
2021-03-02 09:58:23 +0800

    align multi-head attention with pytorch

---
+++ tests/test_attention.py
@@ -0,0 +1,51 @@
+import _init_path
+
+import pdll 
+import pdll.autograd
+import pdll.nn
+import pdll as L 
+
+import torch
+import numpy as np
+import unittest
+import copy
+import os 
+
+class Testing(unittest.TestCase):
+
+    def test_multihead(self, ):
+        ''' '''
+        
+        data = np.random.rand(8, 3, 16)
+
+        dt = torch.tensor(data, requires_grad=True, dtype=torch.float32)
+        mt = torch.nn.MultiheadAttention(16, 4)
+
+        outt, attt = mt(dt, dt, dt)
+        outt.mean().backward()
+
+        print(attt.shape    )
+
+        dl = L.from_numpy(data[...], requires_grad=True)
+        ml = L.nn.MultiHeadAttention(16, 4)
+
+        ml.in_proj_weight.storage[...] = mt.in_proj_weight.data.numpy()
+        ml.in_proj_bias.storage[...] = mt.in_proj_bias.data.numpy()
+        ml.out_proj.weight.storage[...] = mt.out_proj.weight.data.t().numpy()
+        ml.out_proj.bias.storage[...] = mt.out_proj.bias.data.numpy()
+
+        outl, _ = ml(dl, dl, dl)
+        outl.mean().backward()
+
+        print(ml.in_proj_weight.shape, mt.in_proj_weight.shape)
+        print('output', outl.shape)
+
+        np.testing.assert_almost_equal(outl.data.storage, outt.data.numpy(), decimal=4)
+        np.testing.assert_almost_equal(dl.grad.data.storage, dt.grad.data.numpy(), decimal=4)
+
+
+
+if __name__ == '__main__':
+    
+    unittest.main(verbosity=1)
+
